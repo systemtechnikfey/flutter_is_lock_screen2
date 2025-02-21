7be863a1092b9f38349aef2aef1a3da6651c7248
Jan-Henrik <info@systemtechnik-fey.de>
2025-02-21 14:02:53 +0100

    Update IsLockScreenPlugin.kt

--- android/src/main/kotlin/com/wingli/is_lock_screen2/IsLockScreenPlugin.kt
+++ android/src/main/kotlin/com/wingli/is_lock_screen2/IsLockScreenPlugin.kt
@@ -11,7 +11,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** IsLockScreenPlugin */
 public class IsLockScreenPlugin(val registrarContext: Context? = null) : FlutterPlugin, MethodCallHandler {
@@ -26,23 +25,6 @@ public class IsLockScreenPlugin(val registrarContext: Context? = null) : Flutter
     bindingContext = flutterPluginBinding.applicationContext
     channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "is_lock_screen")
     channel.setMethodCallHandler(this)
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "is_lock_screen")
-      channel.setMethodCallHandler(IsLockScreenPlugin(registrarContext = registrar.activeContext()))
-    }
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
